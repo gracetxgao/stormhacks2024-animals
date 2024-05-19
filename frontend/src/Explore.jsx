a26cdea1b6a42e790297db345460823008ab25f8
gracetxgao <101677420+gracetxgao@users.noreply.github.com>
2024-05-19 03:29:00 -0700

    animal styles

--- frontend/src/Explore.jsx
+++ frontend/src/Explore.jsx
@@ -23,9 +23,9 @@ const Explore = () => {
 
     return (
         <Container className="fluid">
-            <div class="row">
-                <div class="col-sm-1"></div>
-                    <div class="col-sm-10" align="center">
+            <div className="row">
+                <div className="col-sm-1"></div>
+                    <div className="col-sm-10" align="center">
                     <Form className="w-100">
                         <Form.Group controlId="exampleForm.ControlInput1">
                             <Form.Label style={{marginTop: 100, marginBottom: 30, fontSize: 40, fontWeight: "bold"}}>Explore!</Form.Label>
@@ -33,11 +33,11 @@ const Explore = () => {
                             <div style={{marginBottom:40}}></div>
                         </Form.Group>
                     </Form>
-                    <Animal name="fu bao" location="china" color="F6E1C1" animal="panda" image="https://upload.wikimedia.org/wikipedia/commons/thumb/0/0f/Grosser_Panda.JPG/1599px-Grosser_Panda.JPG"/>
-                    <Animal name="sandra" location="canada" color="DCF6C1" animal="gorilla" image="https://upload.wikimedia.org/wikipedia/commons/thumb/0/0f/Grosser_Panda.JPG/1599px-Grosser_Panda.JPG"/>
-                    <Animal name="kayla" location="canada" color="F5E0FF" animal="saola" image="https://upload.wikimedia.org/wikipedia/commons/thumb/0/0f/Grosser_Panda.JPG/1599px-Grosser_Panda.JPG"/>
-                    <Animal name="ryan" location="nuggets" color="F6E1C1" animal="rhino" image="https://upload.wikimedia.org/wikipedia/commons/thumb/0/0f/Grosser_Panda.JPG/1599px-Grosser_Panda.JPG"/>
-                    <Animal name="percy" location="nuggets" color="DCF6C1" animal="porpoise" image="https://upload.wikimedia.org/wikipedia/commons/thumb/0/0f/Grosser_Panda.JPG/1599px-Grosser_Panda.JPG"/>
+                    <Animal name="fu bao" location="china" color="F6E1C1" highlightColor="F0C490" animal="panda" image="https://upload.wikimedia.org/wikipedia/commons/thumb/0/0f/Grosser_Panda.JPG/1599px-Grosser_Panda.JPG"/>
+                    <Animal name="sandra" location="canada" color="DCF6C1" highlightColor="ACD37A" animal="gorilla" image="https://upload.wikimedia.org/wikipedia/commons/thumb/0/0f/Grosser_Panda.JPG/1599px-Grosser_Panda.JPG"/>
+                    <Animal name="kayla" location="canada" color="F5E0FF" highlightColor="BA90F0" animal="saola" image="https://upload.wikimedia.org/wikipedia/commons/thumb/0/0f/Grosser_Panda.JPG/1599px-Grosser_Panda.JPG"/>
+                    <Animal name="ryan" location="nuggets" color="F6E1C1" highlightColor="F0C490" animal="rhino" image="https://upload.wikimedia.org/wikipedia/commons/thumb/0/0f/Grosser_Panda.JPG/1599px-Grosser_Panda.JPG"/>
+                    <Animal name="percy" location="nuggets" color="DCF6C1" highlightColor="ACD37A" animal="porpoise" image="https://upload.wikimedia.org/wikipedia/commons/thumb/0/0f/Grosser_Panda.JPG/1599px-Grosser_Panda.JPG"/>
                 </div>
             </div>
         </Container>
